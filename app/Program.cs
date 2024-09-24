30b0782c4ea011a5a6a33e828ef8b629b413278d
Jhonatan-prog <110551929+Jhonatan-prog@users.noreply.github.com>
2024-09-23 20:16:34 -0500

    DB connection organized and successfully connected

--- app/Program.cs
+++ app/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// DESKTOP-QFPIQLQ
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "Server=localhost\\SQLEXPRESS;Database=ConsoleApplicationDB;Trusted_Connection=True;Encrypt=False;";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                // Open the connection
+                connection.Open();
+                Console.WriteLine("Connection opened successfully.");
+
+                string query = "SELECT * FROM Cliente";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {   
+                            Console.WriteLine(reader["codigo"]);
+                            Console.WriteLine(reader["credito"]);
+                            Console.WriteLine(reader["codigo_empresa"]);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                // Closed connection
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
